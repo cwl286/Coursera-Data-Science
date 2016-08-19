6da2cd40a7d76f797e73b99c2e013ec2463949dd
Chris Gomes <ugali_guy@hotmail.com>
2016-08-19 18:52:42 -0500

    Delete station id number column from data set

--- Developing-Data-Products/shiny-app/about.md
+++ Developing-Data-Products/shiny-app/about.md
@@ -1,1 +1,6 @@
-Explain how to use this app.
+# CTA Red Line Ridership in 2015
+
+### Wishlist
+
+* Extend this the entire data set
+* Allow for information to be displayed by train line (not just by station)
--- Developing-Data-Products/shiny-app/ui.R
+++ Developing-Data-Products/shiny-app/ui.R
@@ -17,31 +17,49 @@ train_data <- read.csv("./data/CTA_-_Ridership_-__L__Station_Entries_-_Daily_Tot
 train_data$date <- as.Date(train_data$date, "%m/%d/%Y")
 # Subset the data to entries after the Pink Line opened on June 25 2006
 train_data <- subset(train_data, date >= "2015-01-01" & date < "2016-01-01")
-
+train_data <- subset(train_data, select = -c(ï..station_id) )
 
 # Define UI for application that displays number of passengers at a given station
 shinyUI(
-  fluidPage(
-    titlePanel("CTA Ridership at El Stations in 2015"),
-    
-    # Create a new Row in the UI for selectInputs
-    fluidRow(
-      column(4,
-             selectInput("stat",
-                         "Station:",
-                         c("All",
-                           unique(as.character(train_data$stationname))))
-      ),
-      column(4,
-             selectInput("date",
-                         "Date:",
-                         c("All",
-                           unique(as.character(train_data$date))))
-      )
+  navbarPage("CTA Rail Ridership in 2015",
+    tabPanel("Table",
+             
+            # Create a new Row in the UI for selectInputs
+            fluidRow(
+              column(4,
+                    selectInput("stat",
+                                "Station:",
+                                c("All",
+                                  unique(as.character(train_data$stationname))))
+             ),
+              column(4,
+                    selectInput("date",
+                                "Date:",
+                                c("All",
+                                  unique(as.character(train_data$date))))
+              )
+            ),
+            # Create a new row for the table.
+            fluidRow(
+              DT::dataTableOutput("table")
+              )
+            ),
+    tabPanel("Statisitcs",
+             verbatimTextOutput("summary")
     ),
-    # Create a new row for the table.
-    fluidRow(
-      DT::dataTableOutput("table")
+    tabPanel("About",
+             fluidRow(
+               column(6,
+                      includeMarkdown("about.md")
+               ),
+               column(3,
+                      img(class="img-polaroid",
+                          src=paste0("http://farm8.static.flickr.com/7525/16111190931_3c78bcde8c.jpg")),
+                      tags$small(
+                        a(href="http://farm8.static.flickr.com/7525/16111190931_3c78bcde8c.jpg")
+                      )
+               )
+             )
     )
   )
 )
