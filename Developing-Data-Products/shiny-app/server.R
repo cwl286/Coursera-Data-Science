183ccb0d1ba433fdf48f71e907f9bc0405cec221
Chris Gomes <ugali_guy@hotmail.com>
2016-08-21 01:09:42 -0500

    Submit files for  slidify-deck

--- Developing-Data-Products/shiny-app/server.R
+++ Developing-Data-Products/shiny-app/server.R
@@ -11,6 +11,27 @@ library(shiny)
 library(ggplot2)
 library(DT)
 
+train_data <- read.csv("./data/CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals.csv")
+
+# Rename the levels of factor daytype to something more meaningful
+levels(train_data$daytype) <- c("Saturday", "Sunday/Holiday", "Weekday")
+train_data$date <- as.Date(train_data$date, "%m/%d/%Y")
+
+# Subset the data to entries after the Pink Line opened on June 25 2006
+train_data <- subset(train_data, date >= "2015-01-01" & date < "2016-01-01")
+#train_data <- subset(train_data, select = -c(ï..station_id) )
+
+# Subset the data to entries for Red Line stations
+red_line <- c('Sheridan',  'Morse', 'Sox-35th-Dan Ryan', '79th', 'Grand/State', 'Berwyn', '95th/Dan Ryan', 'Washington/State', 'Wilson', 
+              'Jackson/State', 'Clark/Division', 'North/Clybourn', 'Granville', 'Lawrence', 'Thorndale', 'Howard', '63rd-Dan Ryan', '69th', 'Cermak-Chinatown', 
+              'Monroe/State', 'Garfield-Dan Ryan', 'Jarvis', 'Argyle', 'Fullerton', '47th-Dan Ryan', 'Loyola', 'Belmont-North Main', 'Bryn Mawr', 'Roosevelt', 
+              'Addison-North Main', '87th', 'Chicago/State', 'Harrison', 'Lake/State')
+
+train_data <- train_data[train_data$stationname %in% red_line, ]
+
+format(train_data$date, "%Y-%b-%d")
+months <- format(train_data$date,' %b')
+
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
   
@@ -34,7 +55,12 @@ shinyServer(function(input, output) {
       train_data <- train_data[train_data$date == input$date1,]
     }
     g <- ggplot(data = train_data, aes(train_data$date, train_data$rides))
-    g + geom_point(size=4, alpha= 1/2, aes(color=train_data$daytype)) + geom_smooth(colour="#CC0000") + theme_bw()
+    
+    # Note how multiline ggplot commands are broken up - no leading '+' on new line
+    g + geom_point(size=4, alpha= 1/2, aes(color=train_data$daytype)) + 
+      geom_smooth(colour="#AC0000") + 
+      labs(title= 'CTA Red Line Ridership in 2015', x= 'Date', y= 'Number of Riders')  + 
+      labs(colour = "Type of Day") + theme_bw()
   })
   
 })
